e94b09bd9e2d1c07a821f41cb0047a649cd85ab6
xakyth <xakyth@gmail.com>
2023-07-26 20:09:38 +0300

    added cp1870

---
+++ leetcode/cp1870/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.cp1870;
+
+public class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int l = 0;
+        int r = 10_000_000;
+        int mid = -1;
+        int best = -1;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (enoughSpeed(dist, hour, mid)) {
+                best = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;    
+            }
+        }
+        return best;      
+    }
+    public static boolean enoughSpeed(int[] dist, double hour, int speed) {
+        for (int i = 0; i < dist.length - 1; i++) {
+            hour -= Math.ceil((double)dist[i] / (double)speed);
+        }
+        hour -= (double)dist[dist.length - 1] / (double)speed;
+        if (hour >= -0.0000000001)
+            return true;
+        return false;
+    }
+} 
