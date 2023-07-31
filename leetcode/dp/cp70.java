77f1ecb16801085167784dc0f83c38b37f4288af
xakyth <xakyth@gmail.com>
2023-07-31 21:25:13 +0300

    +dp cp70

---
+++ leetcode/dp/cp70.java
@@ -0,0 +1,19 @@
+package leetcode.dp;
+
+public class cp70 {
+    public int climbStairs(int n) {
+        if (n == 1) 
+            return 1;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        cp70 c = new cp70();
+        System.out.println(c.climbStairs(5));
+    }
+}
